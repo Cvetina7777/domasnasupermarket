6720221e8d43568f532a159ae5928e42c86d24aa
Cvetina7777 <tt70207252@edu1.pgzaimov.com>
2024-06-06 14:43:49 +0300

    Zadachamagazin

---
+++ Product.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketZadacha
+{
+    internal class Product
+    {
+        
+        private string nameofproduct;
+        private decimal price;
+        private int quantity;
+        private decimal totalprice;
+
+        public string ProductName 
+        { get 
+            { 
+                return nameofproduct; 
+            }
+            set
+            {
+                nameofproduct = value;
+            }
+        }
+        public decimal ProductPrice
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                quantity = value;
+            }
+        }
+        public decimal TotalPrice
+        {
+          get 
+            { 
+                return ProductPrice*Quantity; 
+            }
+            set 
+            { 
+                totalprice = value; 
+            }
+        }
+        
+        public Product(string name, decimal price, int quantity)
+        {
+            this.ProductName = name;
+            this.ProductPrice = price;
+            this.Quantity = quantity;
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SupermarketZadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+            string input;
+            int index = 0;
+            List<string> inputs = new List<string>();
+            while ((input = Console.ReadLine()) != "stocked")
+            {
+                inputs.Add(input);
+            }
+
+           
+            for (index = 0; index < inputs.Count; index++)
+            {
+                string[] parts = inputs[index].Split(' ');
+                string name = parts[0];
+                decimal price = decimal.Parse(parts[1]);
+                int quantity = int.Parse(parts[2]);
+
+                if (products.ContainsKey(name))
+                {
+                    products[name].Quantity += quantity;
+                    products[name].ProductPrice = price;
+                }
+                else
+                {
+                    products[name] = new Product(name,price,quantity);
+                }
+            }
+
+            
+            decimal grandTotal = 0;
+            List<string> output = new List<string>();
+            List<string> keys = new List<string>(products.Keys);
+
+            for (index = 0; index < keys.Count; index++)
+            {
+                Product product = products[keys[index]];
+                decimal total = product.TotalPrice;
+                output.Add($"{product.ProductName} {product.ProductPrice:F2} * {product.Quantity} = {total:F2}");
+                grandTotal += total;
+            }
+
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine($"Grand Total: ${grandTotal:F2}");
+            
+
+        }
+    }
+}
